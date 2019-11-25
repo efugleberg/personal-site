7ce1860f90ca80bb04e2bddfea0b8d314b9b64c5
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-11-25 14:13:00 -0600

    contact page finished

--- website/src/components/Bookshelf/Bookshelf.js
+++ website/src/components/Bookshelf/Bookshelf.js
@@ -1,35 +1,33 @@
-import React from 'react';
-import BookList from './BookList.js';
-import Books from './Books.js';
+import React from "react";
+import BookList from "./BookList.js";
+import Books from "./Books.js";
 
 class Bookshelf extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            books: []
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      books: []
+    };
+  }
 
-    componentDidMount() {
-        this.setState({
-            books: BookList
-        })
-    }
+  componentDidMount() {
+    this.setState({
+      books: BookList
+    });
+  }
 
-    render() {
-        return(
-            <div className="Content">
-                <h3>Here is an inventory of the books I own a physical copy of.</h3>
-                <h3>Someday I may organize this list.</h3>
-                <h3>But that day is not today. </h3>
-                {this.state.books.map(book => (
-                <Books key={book.id} book={book} />
-                ))}
-
-
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="Content">
+        <h3>Here is an inventory of the books I own a physical copy of.</h3>
+        <h3>Someday I may organize this list.</h3>
+        <h3>But that day is not today. </h3>
+        {this.state.books.map(book => (
+          <Books key={book.id} book={book} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Bookshelf;
--- website/src/components/Contact/Contact.js
+++ website/src/components/Contact/Contact.js
@@ -1,12 +1,65 @@
-import React from 'react';
+import React from "react";
+import email from "../../assets/Email.png";
+import github from "../../assets/GitHubLg.png";
+import linkedin from "../../assets/LinkedInLg.png";
+import styled from "styled-components";
+
+const ContainerBottom = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  padding: 20px;
+`;
+
+const SpanBottom = styled.span`
+  cursor: pointer;
+  flex-wrap: ;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  padding: 30px;
+`;
 
 const Contact = () => {
-
-    return(
-        <div>
-
-        </div>
-    )
-}
+  return (
+    <div className="Content">
+      <div className="Contact">
+        <h1>CONTACT:</h1>
+        <h3>
+          If there is interest, I plan to assemble distribution list in the
+          future. In the meantime, please feel free to email me at{" "}
+          <a href="mailto: eric.fugleberg@gmail.com">
+            <strong>eric.fugleberg@gmail.com</strong>
+          </a>
+          . There are also links to my LinkedIn and Github profiles. Email will
+          likely summon the quickest response.
+        </h3>
+        <ContainerBottom>
+          <div className="contact-logos">
+            <SpanBottom>
+              <a href="mailto: eric.fugleberg@gmail.com" target="_blank">
+                <img src={email} alt="email" />
+              </a>
+            </SpanBottom>
+            <SpanBottom>
+              <a
+                href="https://www.linkedin.com/in/eric-fugleberg"
+                target="_blank"
+              >
+                <img src={linkedin} alt="linkedin" />
+              </a>
+            </SpanBottom>
+            <SpanBottom>
+              <a href="https://github.com/efugleberg" target="_blank">
+                <img src={github} alt="github" />
+              </a>
+            </SpanBottom>
+          </div>
+        </ContainerBottom>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
