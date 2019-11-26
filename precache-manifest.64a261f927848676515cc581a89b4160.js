1d07092db655effdc54ff23eb71ada19755ae454
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-11-26 15:24:13 -0600

    Updates

---
+++ precache-manifest.64a261f927848676515cc581a89b4160.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e312a6adcb5f8e1da57d9b90708845d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8c123e6c672b798c6d7",
+    "url": "/static/css/2.7aa6f31f.chunk.css"
+  },
+  {
+    "revision": "21db108fab71d1fcb2ea",
+    "url": "/static/css/main.06201ded.chunk.css"
+  },
+  {
+    "revision": "c8c123e6c672b798c6d7",
+    "url": "/static/js/2.d95d8817.chunk.js"
+  },
+  {
+    "revision": "21db108fab71d1fcb2ea",
+    "url": "/static/js/main.ae0aba9d.chunk.js"
+  },
+  {
+    "revision": "4d5b4449eca69dca7740",
+    "url": "/static/js/runtime-main.77d3c816.js"
+  },
+  {
+    "revision": "4c001699a47a4007c9f6922908aa0c3f",
+    "url": "/static/media/ApplePicking.4c001699.jpeg"
+  },
+  {
+    "revision": "15ccee72497a7e9bc8ced857cd790560",
+    "url": "/static/media/Halloween.15ccee72.jpeg"
+  },
+  {
+    "revision": "1027ab767ac9c4545634de4f368d6a8b",
+    "url": "/static/media/Skiing.1027ab76.jpeg"
+  },
+  {
+    "revision": "51825eca0fbad2c31f85707b34af1946",
+    "url": "/static/media/flex.51825eca.png"
+  },
+  {
+    "revision": "40bf07647751aa1cf3c82d4825eadcdf",
+    "url": "/static/media/jasmine.40bf0764.png"
+  }
+]);
